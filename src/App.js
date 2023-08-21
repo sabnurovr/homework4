25b6ce7eb4b281a9a73fd8484c6aac7c55167d9c
sabnurovr <sabnurovv@mail.ru>
2023-08-21 21:46:22 +0600

    tyu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        phoneNumber: '',
+        email: '',
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    return (
+        <div className="registration-form-container">
+            <h2>Регистрация пользователя</h2>
+            <div className="form-group">
+                <label htmlFor="fullName">ФИО</label>
+                <input
+                    type="text"
+                    id="fullName"
+                    name="fullName"
+                    value={formData.fullName}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="phoneNumber">Номер телефона</label>
+                <input
+                    type="text"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email адрес</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <button className="submit-button">Зарегистрироваться</button>
+            </div>
+            <div className="display-data">
+                <h3>Данные пользователя:</h3>
+                <p>ФИО: {formData.fullName}</p>
+                <p>Номер телефона: {formData.phoneNumber}</p>
+                <p>Email: {formData.email}</p>
+            </div>
+        </div>
+    );
+};
+
+export default RegistrationForm;
